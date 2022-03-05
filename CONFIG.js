e9b9aa53343b623f4a4e614a9b457239e1dc07c6
leducanh11072000 <85017146+leducanh11072000@users.noreply.github.com>
2022-03-05 11:14:52 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -4,7 +4,7 @@ const CONFIGDATA = {
     'Cái này là ý tưởng từ 1 cái trò tỏ tình của bọn con trai từ ngày xửa ngày xưa rồi,cụ thể cái tittle này sẽ là "Em đồng ý làm ny anh nhé hay gì đó"</br></br>Nên chắc em sẽ biết thôi</br>Tại anh không hay nghịch mấy cái này lắm, làm thì đơn giản</br></br>Mà ý tưởng thì khó</br></br>Hãy tha thứ cho tâm hồn nhạt nhẽo này',
   buttonYes: "Đồng Ý(Chỉ cái này đứng im cho em click thôi)",
   buttonNo: "Không! Còn lâu nhé.(không click được đâu)",
-  titleModar: "Cảm ơn em vì đồng ý",
+  titleModar: "Là Lá La",
   descriptionModar:
     'Toi quá già cho những trò tỏ tình này rồi, Chúc Minh Ngọc có 1 ngày vui vẻ',
 };
