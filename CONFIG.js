7ad42387f3f1ff2d8788242fc4725d56ea2c10f2
anhldhe141746 <97073334+anhldhe141746@users.noreply.github.com>
2022-03-05 17:14:58 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,7 +1,7 @@
 const CONFIGDATA = {
   titleHeader: "Hơi bị chiều luôn đấy nhé",
   descriptionHeader:
-    'Cái này là ý tưởng từ 1 cái trò tỏ tình của bọn con trai từ ngày xửa ngày xưa rồi,cụ thể cái tittle này sẽ là "Em đồng ý làm ny anh nhé hay gì đó"</br></br>Nên chắc em sẽ biết thôi</br>Tại anh không hay nghịch mấy cái này lắm, làm thì đơn giản</br></br>Mà ý tưởng thì khó</br></br>Hãy tha thứ cho tâm hồn nhạt nhẽo này',
+    'Anh định làm cho em 1 bài thơ cơ</br></br>Nhưng mà lâu rồi không làm :"<< viết xong thì đọc chuối ơi là chuối</br></br>Nên cho em xem tạm, sau anh nghĩ ra gì đấy thì sẽ làm cho em cái khác </br></br>Hãy tha thứ cho tâm hồn nhạt nhẽo này',
   buttonYes: "Đồng Ý(Chỉ cái này đứng im cho em click thôi)",
   buttonNo: "Không! Còn lâu nhé.(không click được đâu)",
   titleModar: "Là Lá La",
